43494b5888b547a85dca9ab2400266ec3b48c6e7
EduardoPaFer <165804466+EduardoPaFer@users.noreply.github.com>
2024-05-07 16:52:20 +0200

    Nuevos obstaculos
    telaraña creada, pared rompible funcional y pruebas en la escena de prueba

--- Assets/Scripts/PlataformaCaida.cs
+++ Assets/Scripts/PlataformaCaida.cs
@@ -6,41 +6,28 @@ using UnityEngine;
 public class PlataformaCaida : MonoBehaviour
 {
     // Start is called before the first frame update
-    float groundCheckDistance = 1f;
-    float groundCheckTimeDistance = 1f;
-    public LayerMask breakableGround;
-    public LayerMask ground;
     int life = 2;
     public BoxCollider2D Ground;
     public SpriteRenderer groundSprite;
+    private bool recentlyDamaged = false;
     void Start()
     {
-        
+        groundSprite.color = Color.white;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, breakableGround))
-        {
-            life = life - 1;
-            Debug.Log("-1 hp ground");
-            groundCheckDistance = -1f;
-        }
         if (life == 0)
         {
             StartCoroutine(Break());
             life = 2;
         }
-        if (Physics2D.Raycast(transform.position, Vector2.down, groundCheckTimeDistance, ground))
-        {
-            groundCheckDistance = 1f;
-        }
-
     }
     IEnumerator Break()
     {
         Debug.Log("breaking...");
+        groundSprite.color = Color.red;
         yield return new WaitForSecondsRealtime(2);
         Ground.enabled = false;
         groundSprite.enabled = false;
@@ -50,7 +37,28 @@ public class PlataformaCaida : MonoBehaviour
     {
         Debug.Log("Respawning platform...");
         yield return new WaitForSecondsRealtime(5);
+        Debug.Log("Respawned ground" + life);
         Ground.enabled = true;
         groundSprite.enabled = true;
+        groundSprite.color = Color.white;
+    }
+    IEnumerator FinishDamage()
+    {
+        yield return new WaitForEndOfFrame();
+        recentlyDamaged = false;
+    }
+    private void OnTriggerEnter2D (Collider2D collision)
+    {
+        if (collision.tag == "Character")
+        {
+            if (!recentlyDamaged)
+            {
+                life = life - 1;
+                Debug.Log("-1 hp ground" + life);
+                groundSprite.color = Color.yellow;
+            }
+            recentlyDamaged = true;
+            StartCoroutine(FinishDamage());
+        }
     }
 }
