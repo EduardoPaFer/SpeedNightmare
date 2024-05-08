35abcbad36e5e74797314adf03098ec903d58478
EduardoPaFer <165804466+EduardoPaFer@users.noreply.github.com>
2024-05-08 15:22:00 +0200

    Cheatcode y final lvl2
    Hardcode uso del 1, 2, 3... para cambiar de lvl

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -1,11 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChange : MonoBehaviour
 {
+    void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SceneManager.LoadScene(1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SceneManager.LoadScene(2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         int currentScene = 0;
