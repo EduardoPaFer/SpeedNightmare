e68465ad6c958efe64fcc06558fe2371129b3f02
EduardoPaFer <165804466+EduardoPaFer@users.noreply.github.com>
2024-05-21 23:36:09 +0200

    Arreglo
    Con esa linea era incapaz de crear una build funcional

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.Build.Reporting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
