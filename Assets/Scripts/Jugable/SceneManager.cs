e752b30ab65926a3cca50dc75abd74ef0cc54f30
EduardoPaFer <165804466+EduardoPaFer@users.noreply.github.com>
2024-05-22 17:05:19 +0200

    Finalización función menú, arreglo hitbox gato

--- Assets/Scripts/Jugable/SceneManager.cs
+++ Assets/Scripts/Jugable/SceneManager.cs
@@ -11,21 +11,25 @@ public class SceneChange : MonoBehaviour
 
     void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (Input.GetKeyDown(KeyCode.Alpha0))
         {
             SceneManager.LoadScene(0);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             SceneManager.LoadScene(1);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha3))
+        if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             SceneManager.LoadScene(2);
         }
-        if (Input.GetKeyDown(KeyCode.Alpha4))
+        if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             SceneManager.LoadScene(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SceneManager.LoadScene(4);
         }
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
